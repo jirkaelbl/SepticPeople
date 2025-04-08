55f999a095fff935c0b1b805f57c70577b5f8205
orlan <orlando515@seznam.cz>
2025-04-08 23:09:29 +0200

    tvorba footer part 4

--- footer.php
+++ footer.php
@@ -1,8 +1,48 @@
-<footer class="mt-auto">
-    <div class="d-flex row bg-black">
+<footer class="mt-auto bg-black">
+    <div class="container-fluid row pt-3 pb-1 justify-content-center mx-auto">
+        <div class="container col-12 col-lg-4 d-flex flex-column align-items-center text-center">
+            <a href="<?php echo home_url(); ?>" class="nav-logo pb-3">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/septic_people_logo.jpg" alt="Logo" loading="lazy">
+            </a>
 
+            <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-2 mt-4 mb-3 w-100">
+                <button type="button" class="btn btn-primary kontakt-button fs-5" style="width: 180px;">Pro pořadatele</button>
+                <button type="button" class="btn btn-primary kontakt-button fs-5 px-3" style="width: 180px;">Koncerty</button>
+            </div>
+        </div>
+        <div class="col-lg-4">
+
+        </div>
+        <div class="col-lg-4">
+
+        </div>
     </div>
-    <div class="container-fluid text-center bg-black">
+
+    <div class="container-fluid footer-line">
+        <div class="d-flex flex-column flex-lg-row pt-4 pb-2 justify-content-center align-items-center">
+            <div>
+                <a class="text-decoration-none pt-lg-0 pt-3 pb-lg-0 pb-3" href="https://www.facebook.com/septicpeople">
+                    <i class="fa-brands fa-facebook-f social-icon"></i>
+                </a>
+                <a class="text-decoration-none pt-lg-0 pt-3 pb-lg-0 pb-3" href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Fseptic_people%3Figsh%3DMWF4MXN3NHNqbjV5bw%253D%253D%26fbclid%3DIwZXh0bgNhZW0CMTAAAR5Cd6a4qZVI-1T_OoIOcAe0r9pcRP-u-5VJ6csNgKf3-ff-PhJDAnyDSpIyjg_aem_o6p7GC9GbJt424OzLihzqQ&h=AT3vmYaic0YiwUxy3vk4aj9D6oBSkoTXHPJ062Vobyxah6DW6z8M9Eo3smYxSH2W2OFm8tX5d0SCATjB-irXKzQa9ZuCyC_j1MKZvJ21QbUe-SuRfwjxz_M3uxTqjlhMt_gp5Q">
+                    <i class="fa-brands fa-instagram social-icon"></i>
+                </a>
+                <a class="text-decoration-none pt-lg-0 pt-3 pb-lg-0 pb-3" href="#">
+                    <i class="fa-solid fa-envelope social-icon"></i>
+                </a>
+            </div>
+            <div class="mt-lg-0 mt-3">
+                <a class="text-decoration-none pt-lg-0 pt-3 pb-lg-0 pb-3" href="#">
+                    <i class="fa-brands fa-youtube social-icon"></i>
+                </a>
+                <a class="text-decoration-none pt-lg-0 pt-3 pb-lg-0 pb-3" href="#">
+                    <i class="fa-brands fa-tiktok social-icon"></i>
+                </a>
+            </div>
+
+        </div>
+    </div>
+    <div class="container-fluid text-center bg-black footer-line">
         <p class="text-white mb-0 pt-3 pb-1">&copy; <?php echo "Designed and hosted by"; ?><a class="text-decoration-none helgroup-footer" href="https://helgroup.cz/"> Helgroup</a></p>
         <p class="text-white pb-3 mb-0">&copy; <?php echo date("Y"); ?></p>
     </div>
--- functions.php
+++ functions.php
@@ -27,21 +27,24 @@ add_action( 'wp_enqueue_scripts', 'wpdocs_my_enqueue_scripts' );
 function wpdocs_my_enqueue_scripts() : void {
     // Enqueue my styles.
     wp_enqueue_style( 'aos-style', 'https://unpkg.com/aos@next/dist/aos.css' );
-    // wp_enqueue_style( 'owl-carousel-style', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css' );
     wp_enqueue_style( 'animate-style', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' );
     wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' );
     wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' );
 
-    // Enqueue my scripts.
-    // wp_enqueue_script( 'gsap', get_template_directory_uri() . '/js/app.js', array(), '1.0.0', true );
-    // wp_enqueue_script( 'navigace', get_template_directory_uri() . '/js/nav.js', array(), '1.0.0', true );
+    // Enqueue your main style (this will be used for inline styles).
+    wp_enqueue_style( 'main-style', get_template_directory_uri() . '/css/style.css' );
+
+    // Scripts
     wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js' );
     wp_enqueue_script( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js' );
+}
 
-    wp_enqueue_style( 'main-style', get_template_directory_uri() . '/css/style.css', true);
-
-    // Načtení fontu WinkySans z googlu
+// VYTAŽENO VEN:
+add_action('wp_enqueue_scripts', 'custom_fonts_styles');
+function custom_fonts_styles() {
     $font_url1 = get_template_directory_uri() . '/assets/font_winky_sans/static/WinkySans-Regular.ttf';
+    $font_url2 = get_template_directory_uri() . '/assets/font_winky_sans/static/DirtyBoy.ttf';
+
     $custom_css = "
         @font-face {
             font-family: 'WinkySans';
@@ -49,23 +52,24 @@ function wpdocs_my_enqueue_scripts() : void {
             font-weight: normal;
             font-style: normal;
         }
-        body {
-            font-family: 'WinkySans', sans-serif;
-        }
-    ";
 
-    $font_url2 = get_template_directory_uri() . '/assets/font_winky_sans/static/DirtyBoy.ttf';
-    $custom_css = "
         @font-face {
             font-family: 'DirtyBoy';
             src: url('$font_url2') format('truetype');
             font-weight: normal;
             font-style: normal;
         }
+
+        body {
+            font-family: 'WinkySans', sans-serif;
+        }
+
+        .hero-text h1 {
+            font-family: 'DirtyBoy', sans-serif;
+        }
     ";
 
-    wp_add_inline_style( 'main-style', $custom_css );
-
+    wp_add_inline_style('main-style', $custom_css);
 }
 
 
